18bcd98ac8ee82804e92c4eeeb429fd7fd549a26
Srivatsa V <138861827+vatsa01@users.noreply.github.com>
2023-08-11 00:05:59 +0530

    Created

---
+++ Part-B/Question-2.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from functools import reduce
+
+
+def word_count(fname):
+    with open(fname) as f:
+        return Counter(f.read().split())
+
+
+d = word_count("lose_yourself.txt")
+print(d)
+# print(type(d))
+lis = list(d.items())
+# print(lis)
+lis.sort(reverse=True, key=lambda x: x[1])
+print(lis)
+print("\nThe top 10 most occurred words are:")
+lis2 = []
+for i in range(10):
+    print(lis[i][0], "\n")
+    lis2.append(len(lis[i][0]))
+print(lis2)
+avg = (reduce(lambda a, b: a + b, lis2)) / len(lis2)
+print("Average is :", avg)
+lis2 = [x * x for x in lis2 if x % 2 != 0]
+print("Final list = ", lis2)
